a5818ced23af3aeb4837584dace297be71fa5321
Bernardo Rodrigues <92566713+b3rnardorm@users.noreply.github.com>
2023-02-06 21:30:23 -0300

    editei o ex015

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -8,6 +8,42 @@ function verificar(){
     }else {
         var fsex = document.getElementsByName('radiosex')
         var idade = ano - Number(fano.value)
-        res.innerHTML = `Idade calculada: ${idade}`
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            genero = 'Homem'
+            if (idade >= 0 && idade < 10){
+                //Criança
+                img.setAttribute('src', 'Imagens/foto-bebe-m.png')
+            } else if (idade < 21){
+                //Jovem
+                img.setAttribute('src', 'Imagens/foto-jovem-m.png')
+            }else if (idade < 50){
+                //Adulto
+                img.setAttribute('src', 'Imagens/foto-adulto-m.png')
+            }else{
+                //Idoso
+                img.setAttribute('src', 'Imagens/foto-idoso-m.png')
+            }
+        } else {
+            genero = 'Mulher'
+            if (idade >= 0 && idade < 10){
+                //Criança
+                img.setAttribute('src', 'Imagens/foto-bebe-f.png')
+            } else if (idade < 21){
+                //Jovem
+                img.setAttribute('src', 'Imagens/foto-jovem-f.png')
+            }else if (idade < 50){
+                //Adulta
+                img.setAttribute('src', 'Imagens/foto-adulto-f.png')
+            }else{
+                //Idosa
+                img.setAttribute('src', 'Imagens/foto-idoso-f.png')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
     }
 }
