a32df24ab411133311d9a2eb901ca6aaaa62a6dd
b3rnardorm <92566713+b3rnardorm@users.noreply.github.com>
2023-02-22 10:39:29 -0300

    criei o ex017

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,15 @@
-alert('Olá')
+function verificar(){
+    var num = document.getElementById('txtnum')
+    var res = document.getElementById('res')
+
+    if (num.value.length == 0){
+        res.innerHTML = '[ERRO] Insira um numero no campo indicado!'
+    }else{
+        res.innerHTML = ''
+        valor = Number(num.value)
+        for (c = 1; c <= 10; c++){
+            mult = valor * c
+            res.innerHTML += `${valor} * ${c} = ${mult} <br>`
+        }
+    }
+}
